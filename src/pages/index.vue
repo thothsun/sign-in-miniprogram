2519888ca0c47c526ffff3024b295788f99a49c1
sunshuai <438819781@qq.com>
2019-05-22 09:42:53 +0800

    完成 基本流程的逻辑

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,19 +5,20 @@
     <view style="min-height: 300px">
 
       <div>
-        <p>姓名：张三</p>
-        <p>学号：20151234</p>
+
+        <p style="font-size: 16px">学号：</p>
+        <input placeholder="请输入学号" v-model="input_id" style="margin: 5px"/>
+        <p style="font-size: 16px">姓名:</p>
+        <input placeholder="请输入姓名" v-model="input_name" style="margin: 5px;"/>
+
       </div>
 
-      <!--<div style="width: 50%">-->
-      <!--<button size="mini">修改</button>-->
-      <!--</div>-->
 
     </view>
 
 
     <view>
-      <button type="primary" open-type="getUserInfo" lang="zh_CN"
+      <button type="primary" open-type="getUserInfo" lang="zh_CN" style="margin-top: 20px;"
               @getuserinfo="onGotUserInfo">签到
       </button>
     </view>
@@ -32,14 +33,43 @@
 
   export default {
     data() {
-      return {}
+      return {
+        input_id: null,
+        input_name: null,
+
+      }
     },
 
+    components: {
+    },
 
     methods: {
       onGotUserInfo(e) {
-        console.log(e.mp.detail.signature)
-        this.scanCodeSignIn()
+        console.log(e.mp);
+        console.log(e.mp.detail.signature);
+        if (this.input_id === null || this.input_name === null) {
+          wx.showToast({
+            title: '请填写完整',
+            icon: 'none',
+            duration: 2000
+          })
+        } else {
+          this.showWarn()
+        }
+      },
+      showWarn() {
+        wx.showModal({
+          title: '提示',
+          content: '每个微信号只能签到一次，请仔细检查个人信息是否正确',
+          success: res => {
+            if (res.confirm) {
+              console.log('用户点击确定');
+              this.scanCodeSignIn()
+            } else if (res.cancel) {
+              console.log('用户点击取消')
+            }
+          }
+        })
       },
       scanCodeSignIn() {
         // 只允许从相机扫码
